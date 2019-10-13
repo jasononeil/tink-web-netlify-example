ab38c8ff11cecf628bba9b6bd10319665129f48f
Jason O'Neil <jason.oneil@gmail.com>
2019-10-13 21:13:07 +0800

    Bypass tink_web for a simpler test of the functions

--- src/NetlifyFunction.hx
+++ src/NetlifyFunction.hx
@@ -7,9 +7,13 @@ import tink.http.Handler;
 class NetlifyFunction {
 	@:expose('handler')
 	static function handler(event, context, callback) {
-		var container = new AwsLambdaNodeContainer(event, context, callback);
-		var router = new Router<Root>(new Root());
-		var handler:Handler = req -> router.route(Context.ofRequest(req)).recover(OutgoingResponse.reportError);
-		container.run(handler).eager();
+		// var container = new AwsLambdaNodeContainer(event, context, callback);
+		// var router = new Router<Root>(new Root());
+		// var handler:Handler = req -> router.route(Context.ofRequest(req)).recover(OutgoingResponse.reportError);
+		// container.run(handler).eager();
+		callback(null, {
+			statusCode: 200,
+			body: 'does this work?'
+		});
 	}
 }
